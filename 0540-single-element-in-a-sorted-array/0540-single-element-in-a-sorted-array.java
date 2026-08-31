b3b17e1bf4a5eac765adee2786a82c53d585ce08
varung202005 <varung202005@gmail.com>
2026-08-31 10:18:06 +0530

    Time: 2 ms (9.15%), Space: 52.7 MB (90.35%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]){
+                return nums[i];
+            }
+        }
+        if(nums[0]==nums[1]){
+            return nums[nums.length-1];
+        }
+        return nums[0];
+    }
+}
